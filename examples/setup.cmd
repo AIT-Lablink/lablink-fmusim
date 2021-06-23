3c9e5a62daf98b2a54b6c8e6ffc7107da1ffd1ca
Edmund Widl <edmund.e.widl@ieee.org>
2021-06-23 16:29:58 +0200

    Update pom.xml for deployment to Maven Central Repository; update version numbers for Lablink dependencies;

--- examples/setup.cmd
+++ examples/setup.cmd
@@ -23,10 +23,10 @@ REM Path to Java JAR file of data point bridge.
 SET DPB_JAR_FILE=%FMUSIM_ROOT_DIR%\target\dependency\dpbridge-0.0.1-jar-with-dependencies.jar
 
 REM Path to Java JAR file of data plotter.
-SET PLOT_JAR_FILE=%FMUSIM_ROOT_DIR%\target\dependency\plotter-0.0.1-jar-with-dependencies.jar
+SET PLOT_JAR_FILE=%FMUSIM_ROOT_DIR%\target\dependency\plotter-0.0.2-jar-with-dependencies.jar
 
 REM Path to Java JAR file of config server.
-SET CONFIG_JAR_FILE=%FMUSIM_ROOT_DIR%\target\dependency\config-0.0.1-jar-with-dependencies.jar
+SET CONFIG_JAR_FILE=%FMUSIM_ROOT_DIR%\target\dependency\config-0.1.0-jar-with-dependencies.jar
 
 REM Check if environment variable JAVA_HOME has been defined.
 IF NOT DEFINED JAVA_HOME (
